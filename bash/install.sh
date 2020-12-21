6ba17b0db64a2831295b1ddbe6c6e4d10d0d632c
BillGatesCat <>
2020-12-20 22:25:54 -0800

    update installation shell script

--- bash/install.sh
+++ bash/install.sh
@@ -6,5 +6,8 @@ sudo apt install pip
 pip install pyinstaller
 pip install yfinance
 
+git clone https://github.com/sstephenson/bats.git
+./bats/install.sh /usr/local
 
 pyinstaller src/main.py --onefile
+

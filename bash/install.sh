c1442ab6bb2ee07f6efc21a1740cddbe87d04b31
Kieran <kieran@pop-os.localdomain>
2020-12-22 18:53:30 -0800

    update installation scripts

--- bash/install.sh
+++ bash/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 cd $(dirname "$0")
-PREFIX="$1"
+
+./clean.sh
 
 sudo apt install pip
 pip install pyinstaller
 pip install yfinance
-
 git clone https://github.com/sstephenson/bats.git
+
 sudo ./bats/install.sh /usr/local
 
 pyinstaller src/main.py --onefile -n yf
@@ -17,5 +18,5 @@ mkdir libexec bin
 cp dist/yf libexec
 ln -s ../libexec/yf bin
 
-sudo cp -R bin/* "$PREFIX"/bin
-sudo cp -R libexec/* "$PREFIX"/libexec
+sudo cp -R ./bin/* /usr/local/bin
+sudo cp -R ./libexec/* /usr/local/libexec
